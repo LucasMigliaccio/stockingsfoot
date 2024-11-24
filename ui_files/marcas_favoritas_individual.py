ff179e71fc374449594b9e3e3e81b7f5b0e0229e
LucasMigliaccio <lucasmigliaccio10@gmail.com>
2024-11-24 19:15:19 -0300

    commit antes de cambiar rutas por archivo qrc iconos

--- ui_files/marcas_favoritas_individual.py
+++ ui_files/marcas_favoritas_individual.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'calzados_marcas_favoritas.ui'
+## Form generated from reading UI file 'marcas_favoritas_individual.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.7.2
 ##
@@ -18,12 +18,13 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
 from PySide6.QtWidgets import (QApplication, QFrame, QHBoxLayout, QHeaderView,
     QLabel, QSizePolicy, QTableView, QToolButton,
     QVBoxLayout, QWidget)
+import icons_rc
 
-class ViewCalzadosMarcas(object):
+class Ui_ViewEmpleado(object):
     def setupUi(self, ViewEmpleado):
         if not ViewEmpleado.objectName():
             ViewEmpleado.setObjectName(u"ViewEmpleado")
-        ViewEmpleado.resize(989, 745)
+        ViewEmpleado.resize(947, 574)
         self.verticalLayout = QVBoxLayout(ViewEmpleado)
         self.verticalLayout.setSpacing(0)
         self.verticalLayout.setObjectName(u"verticalLayout")
@@ -85,25 +86,25 @@ class ViewCalzadosMarcas(object):
         self.minimize_button.setObjectName(u"minimize_button")
         self.minimize_button.setGeometry(QRect(0, 0, 22, 22))
         icon = QIcon()
-        icon.addFile(u"./assets/icons/minimize-window.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        icon.addFile(u":/newPrefix/assets/icons/minimize-window.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
         self.minimize_button.setIcon(icon)
         self.restore_button = QToolButton(self.butttons_holder_frame)
         self.restore_button.setObjectName(u"restore_button")
         self.restore_button.setGeometry(QRect(30, 0, 22, 22))
         icon1 = QIcon()
-        icon1.addFile(u"./assets/icons/restore-window.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        icon1.addFile(u":/newPrefix/assets/icons/restore-window.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
         self.restore_button.setIcon(icon1)
         self.maximize_button = QToolButton(self.butttons_holder_frame)
         self.maximize_button.setObjectName(u"maximize_button")
         self.maximize_button.setGeometry(QRect(30, 0, 22, 22))
         icon2 = QIcon()
-        icon2.addFile(u"./assets/icons/maximize-window.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        icon2.addFile(u":/newPrefix/assets/icons/maximize-window.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
         self.maximize_button.setIcon(icon2)
         self.close_button = QToolButton(self.butttons_holder_frame)
         self.close_button.setObjectName(u"close_button")
         self.close_button.setGeometry(QRect(60, 0, 22, 22))
         icon3 = QIcon()
-        icon3.addFile(u"./assets/icons/close-window.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        icon3.addFile(u":/newPrefix/assets/icons/close-window.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
         self.close_button.setIcon(icon3)
 
         self.horizontalLayout_6.addWidget(self.butttons_holder_frame)
@@ -124,14 +125,6 @@ class ViewCalzadosMarcas(object):
         self.frame.setFrameShadow(QFrame.Raised)
         self.horizontalLayout = QHBoxLayout(self.frame)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.calzados_table = QTableView(self.frame)
-        self.calzados_table.setObjectName(u"calzados_table")
-        self.calzados_table.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
-"color: rgb(0, 0, 0);\n"
-"border-radius: 10px;")
-
-        self.horizontalLayout.addWidget(self.calzados_table)
-
         self.marcas_table = QTableView(self.frame)
         self.marcas_table.setObjectName(u"marcas_table")
         self.marcas_table.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
@@ -181,7 +174,7 @@ class ViewCalzadosMarcas(object):
 
     def retranslateUi(self, ViewEmpleado):
         ViewEmpleado.setWindowTitle(QCoreApplication.translate("ViewEmpleado", u"Form", None))
-        self.label_tittle.setText(QCoreApplication.translate("ViewEmpleado", u"Calzados Favoritos / Marcas Favoritas", None))
+        self.label_tittle.setText(QCoreApplication.translate("ViewEmpleado", u" Marcas Favoritas por Grupo de Ventas", None))
         self.minimize_button.setText(QCoreApplication.translate("ViewEmpleado", u"...", None))
         self.restore_button.setText(QCoreApplication.translate("ViewEmpleado", u"...", None))
         self.maximize_button.setText(QCoreApplication.translate("ViewEmpleado", u"...", None))
